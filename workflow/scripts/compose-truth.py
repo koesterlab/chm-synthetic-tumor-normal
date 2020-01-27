e3713652598d00b9f95181af04b0f9869db1036d
Johannes Köster <johannes.koester@tu-dortmund.de>
2020-01-27 13:35:42 +0100

    add confident regions bed to output

--- workflow/scripts/compose-truth.py
+++ workflow/scripts/compose-truth.py
@@ -2,7 +2,8 @@ from cyvcf2 import VCF, Writer
 
 vcf_in = VCF(snakemake.input[0])
 vcf_in.add_info_to_header({"ID": "SOMATIC", "Number": 0, "Type": "Flag", "Description": "Somatic variant"})
-vcf_in.add_info_to_header({"ID": "VAF", "Number": 1, "Type": "Float", "Description": "Variant allele frequency"})
+vcf_in.add_info_to_header({"ID": "TAF", "Number": 1, "Type": "Float", "Description": "Tumor allele frequency"})
+vcf_in.add_info_to_header({"ID": "NAF", "Number": 1, "Type": "Float", "Description": "Normal allele frequency"})
 
 vcf_out = Writer(snakemake.output[0], vcf_in)
 
@@ -12,7 +13,13 @@ for rec in vcf_in:
     if rec.genotypes == [[1, 0, True]]:
         # if in CHM1 only, consider as somatic
         rec.INFO["SOMATIC"] = True
-        rec.INFO["VAF"] = vaf
+        rec.INFO["TAF"] = vaf
+        rec.INFO["NAF"] = 0.0
+    elif rec.genotypes == [[1, 1, True]] or rec.genotypes == [[0, 1, True]]:
+        rec.INFO["NAF"] = 1.0
+        rec.INFO["TAF"] = 0.0
+    else:
+        continue
     vcf_out.write_record(rec)
 
 vcf_out.close()
